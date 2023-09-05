aaa2f1330e86b978e19235e1f30082953b091a30
KuwaTakushi <richardlawrence4455@gmail.com>
2023-09-05 11:57:28 +0800

    update

--- App.tsx
+++ App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import GithubCorner from 'react-github-corner';
 import './App.scss';
-
 import { animationDuration, gridGap } from './config';
 import Header from './components/Header';
 import Board from './components/Board';
 //import Info from './components/Info';
 import BoardSizePicker from './components/BoardSizePicker';
 import Rank from './components/Rank';
-import Sign from './components/Sign';
 
 const App: React.FC = () => {
   return (
@@ -21,14 +18,11 @@ const App: React.FC = () => {
         } as any
       }
     >
-      <GithubCorner href="https://github.com/mat-sz/2048" />
       <div className="page">
         <Header />
         <Board />
         <BoardSizePicker />
-        {/** features: 新增用户登录 */}
-        <Sign />
-        {/** features: 新增排行榜面板并初始化 */}
+        {/** feat: Add leaderboard panel and initialize */}
         <Rank />
       </div>
     </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,12 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { resetAction, undoAction } from '../actions';
 import { StateType } from '../reducers';
+import FiscoNetworkServer from '../@fiscobcos-web3/fiscobcos-web3-link/network';
+import { CALL_FUNC_NAME, CONTRACT_ADDRESS_TO, DEFAULT_CALLER, GROUP_ID, RANK_2048_GAME_ABI } from '../config';
+import { decodeContractData, encodeContractData } from '../@fiscobcos-web3/fiscobcos-web3-utils/encode';
+import { TransactionResponse } from '../@fiscobcos-web3/fiscobcos-web3-server/types';
+import { hexArrayToNumber } from '../@fiscobcos-web3/fiscobcos-web3-utils/utils';
 
 const Header: React.FC = () => {
   const dispatch = useDispatch();
@@ -13,6 +18,34 @@ const Header: React.FC = () => {
   const moveId = useSelector((state: StateType) => state.moveId);
   const best = useSelector((state: StateType) => state.best);
   const previousBoard = useSelector((state: StateType) => state.previousBoard);
+
+  const [TopPlayerSCore, setTopPlayerSCore] = useState('');
+  
+  const fetchTopPlayerScore = async () => {
+    try {
+        const server = new FiscoNetworkServer("http://127.0.0.1:3002/fiscobcos/call", GROUP_ID);
+        const data:TransactionResponse = await server.call(
+            DEFAULT_CALLER,
+            CONTRACT_ADDRESS_TO, 
+            '', // empty value, do no send
+            encodeContractData(RANK_2048_GAME_ABI, CALL_FUNC_NAME.getAllPlayersScore),
+        );
+        const decodeData = decodeContractData(
+            RANK_2048_GAME_ABI,
+            'getAllPlayersScore',
+            data.result['output'],
+        );
+        const [_, scores] = decodeData;
+        const topScores = Math.max(...hexArrayToNumber(scores)).toString();
+        setTopPlayerSCore(topScores);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+  useEffect(() => {
+    fetchTopPlayerScore();
+  }, []); 
 
   return (
     <div className="header">
@@ -33,13 +66,9 @@ const Header: React.FC = () => {
             <div>{best}</div>
           </div>
           <div className="header-scores-score">
-            {
-            /** TODO:
-             * 显示最高分的玩家数据
-             */
-            }
+            {/** Display player data with the highest score */}
             <div>Top Player Score</div>
-            <div>{best}</div>
+            <div>{TopPlayerSCore !== '' ? TopPlayerSCore : best}</div>
           </div>
         </div>
       </div>
